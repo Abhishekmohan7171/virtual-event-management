499ef1bd0e7b1758d4d0800ff99d811d961b7541
@abhishek.mohan <abhishek.mohan@surboard.se>
2024-11-19 14:12:24 +0530

    delete and update event admin  routes added

--- models/events.js
+++ models/events.js
@@ -28,3 +28,22 @@ const eventSchema = new mongoose.Schema({
 
 const Event = mongoose.model('Event', eventSchema);
 module.exports = Event;
+
+// {
+//     "_id": "673b91cb782e6ae1d4c3fc6e",
+//     "name": "Coldplay Music of Spheres",
+//     "date": "04-10-2022",
+//     "time": "10:00:00",
+//     "location": "Mumbai",
+//     "description": "Best concert in India till date.",
+//     "attendees": [],
+//     "__v": 0
+// }
+
+// {
+//     "name":"Motoverse",
+//     "date": "23-11-2024 to 25-11-2024",
+//     "time":"10:00",
+//     "location":"Goa",
+//     "description":"Bikers getogether"
+// }
--- routes/events.js
+++ routes/events.js
@@ -15,19 +15,32 @@ router.post("/create",validateJWT, async (req,res) => {
 });
 
 //update an event
-// router.put("/update/:id", (req,res) => {
-//     if(req.user.role === "admin"){
-//         const id = req.params.id;
-
-//     }else{
-//         res.status(401).send({message: "Unauthorized"});
-//     }
-// })
+router.put("/:id",validateJWT, async (req,res) => {
+    if(req.user.role === "admin"){
+        const id = req.params.id;
+        const dbEvent = await Event.findByIdAndUpdate(id,req.body, { new: true });
+        console.log(dbEvent,"->>>>>>>>>>>>>>")
+        res.send({updatedEvent: req.body, message: "Event updated successfully"});
+    }else{
+        res.status(401).send({message: "Unauthorized"});
+    }
+})
 
 //get all events
 router.get("/", async (req,res) => {
     const events = await Event.find();
     res.send({events: events, message: "All Events fetched successfully"});
+})
+
+//delete an event
+router.delete("/:id", validateJWT , async (req,res) => {
+    if(req.user.role === "admin"){
+        const id = req.params.id;
+        await Event.findByIdAndDelete(id);
+        res.send({message: "Event deleted successfully"});
+    }else{
+        res.status(401).send({message: "Unauthorized"});
+    }
 })
 
 module.exports = router;
