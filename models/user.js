4fca0a99184f258d9ab704a9caf0199b9d087895
@abhishek.mohan <abhishek.mohan@surboard.se>
2024-11-20 15:43:00 +0530

    chore: removed unwanted data

--- models/user.js
+++ models/user.js
@@ -35,26 +35,3 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model("User", userSchema);
 module.exports = User;
 
-// {
-//     "name":"Abhishek",
-//     "email":"a@a.com",
-//     "password":"qwerty123",
-//     "role":"user"
-// }
-// {
-//     "name":"Abhiram",
-//     "email":"ar@a.com",
-//     "password":"asdfd123",
-//     "role":"admin"
-// }
-// {
-//     "name":"Greeshma",
-//     "email":"g@a.com",
-//     "password":"greeshma123"
-// }
-// {
-//     "name":"Abhishek M",
-//     "email":"abhishek.mohan@surfboard.se",
-//     "password":"Abhi123",
-//     "role":"user"
-// }
